20e46e3b6f2aa630e102f1d7354a9db11f7aecaa
walessandrini <walessandrini@gmail.com>
2024-07-29 09:27:19 -0300

    removing ipv6

--- vpc.tf
+++ vpc.tf
@@ -70,7 +70,6 @@ resource "aws_security_group" "sg_public_instance" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
